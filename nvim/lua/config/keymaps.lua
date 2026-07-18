af53ee44f4a5cd66b0aba9a40ede640c0ddb3d07
Austin Sofaer <164747892+fraeso@users.noreply.github.com>
2026-07-18 18:37:23 +0700

    remove fff nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,7 +4,3 @@
 vim.keymap.set("n", "<leader>rn", ":IncRename ")
 -- override LazyVim's snacks git_log_file with diffview file history
 vim.keymap.set("n", "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", { desc = "File History (current file)" })
--- use fff.nvim for find-files instead of the snacks picker
-vim.keymap.set("n", "<leader>ff", function() require("fff").find_files() end, { desc = "Find Files (fff)" })
-vim.keymap.set("n", "<leader><space>", function() require("fff").find_files() end, { desc = "Find Files (fff)" })
-vim.keymap.set("n", "<leader>fF", function() require("fff").find_files_in_dir(vim.uv.cwd()) end, { desc = "Find Files cwd (fff)" })
--- nvim/lua/plugins/fff.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "dmtrKovalenko/fff.nvim",
-  build = function()
-    require("fff.download").download_or_build_binary()
-  end,
-  lazy = false,
-  opts = {},
-}
