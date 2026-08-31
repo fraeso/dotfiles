43374de67333952d49fe9469c8567c3379cb2030
Austin Sofaer <164747892+fraeso@users.noreply.github.com>
2026-08-31 20:37:42 +1000

    fix: highlight groups

--- nvim/lua/plugins/colours.lua
+++ nvim/lua/plugins/colours.lua
@@ -72,6 +72,8 @@ return {
         -- NOTE: Highlight groups are extended (merged) by default. Disable this
         -- per group via `inherit = false`
         highlight_groups = {
+          BlinkCmpDoc = { bg = "NONE" },
+          BlinkCmpDocSeparator = { bg = "NONE" },
           -- Comment = { fg = "foam" },
           -- StatusLine = { fg = "love", bg = "love", blend = 15 },
           -- VertSplit = { fg = "muted", bg = "muted" },
